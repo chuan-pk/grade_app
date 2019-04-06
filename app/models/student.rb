5109c00083444d108e66d02d5c220735cfd9722f
Chuan Khuna <chuan_8297@hotmail.com>
2019-04-06 15:55:01 +0700

    add gpax function in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,4 +8,16 @@ class Student < ApplicationRecord
   validates :student_id, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates_confirmation_of :password
+
+  @@letter_grade = {'A'=> 4, 'B+'=> 3.5, 'B'=> 3, 'C+'=> 2.5, 'C'=> 2, 'D+'=> 1.5, 'D'=> '1', 'F'=> 0}
+  
+  def gpax
+    sum_credit = Attempt.where(student_id: self.student_id).joins(:course).sum(:credit)
+    sum_product = 0
+    Attempt.where(student_id: self.student_id).joins(:course).each do |a|
+      sum_product += @@letter_grade[a.grade] * a.course.credit
+    end
+    return (sum_product/sum_credit).to_s[0..3]
+  end
+  
 end
