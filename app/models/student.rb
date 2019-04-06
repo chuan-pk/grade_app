411edc5764ca622ab9fb443b72ec92ffb135790a
Chuan Khuna <chuan_8297@hotmail.com>
2019-04-06 16:25:40 +0700

    add calculate gpa function in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,7 +17,23 @@ class Student < ApplicationRecord
     Attempt.where(student_id: self.student_id).joins(:course).each do |a|
       sum_product += @@letter_grade[a.grade] * a.course.credit
     end
-    return (sum_product/sum_credit).to_s[0..3]
+    return (sum_product/sum_credit).floor(2)
   end
-  
+
+  def gpa 
+    gpa = {}
+    Attempt.where(student_id: self.student_id).group_by(&:year).each do |year, year_attempt|
+      year_attempt.group_by(&:semester).each do |semester, semester_attempt|
+        sum_credit = 0
+        sum_product = 0
+        semester_attempt.each do |a|
+          sum_credit += Course.find(a.course_id).credit
+          sum_product += Course.find(a.course_id).credit * @@letter_grade[a.grade]
+        end
+        gaps.merge!("#{year}/#{semester}"=> (sum_product/sum_credit).floor(2)) 
+      end
+    end
+    gpa
+  end
+
 end
